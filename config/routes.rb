ef661ea540f38c2abd9d6f3b7e385ec6a7b0aceb
Mat Carey <mat@matcarey.co.uk>
2013-12-20 20:34:27 +0000

    Domain testing on rails, not apache.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Tabsnotspaces::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  constraints(:host => /www\..*/) do
+    match "/(*path)" => redirect {|params, req| "http://tabsnotspaces.com#{params[:path]}"},  via: [:get]
+  end
+
   # You can have the root of your site routed with "root"
   root 'welcome#index'
 
