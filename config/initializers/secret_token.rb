076bdc303b8d2f4f9d07ea7db8a84c55e2ce13be
Mat Carey <mat@matcarey.co.uk>
2013-12-20 20:01:27 +0000

    Set up rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tabsnotspaces::Application.config.secret_key_base = 'a0e454f0a08a996e8a976e47513cac9b69be580089bc2b6d307724d14bd260e7905377a28cff8c47dc0f8727ec22112f66384064ff9aa6483bd64141e73a4718'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Tabsnotspaces::Application.config.session_store :cookie_store, key: '_tabsnotspaces_session'
